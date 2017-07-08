b00c15a7ea2983c8b99452dfda2da55a7c3744e8
Huyin2017 <287317075@qq.com>
2017-07-08 17:24:01 +0800

    Update 04_predict.py

--- 04_predict.py
+++ 04_predict.py
@@ -1,11 +1,4 @@
-
 # coding: utf-8
-
-# 51CTO课程频道：http://edu.51cto.com/lecturer/index/user_id-12330098.html<br>
-# 优酷频道：http://i.youku.com/sdxxqbf<br>
-# 微信公众号：深度学习与神经网络<br>
-# Github：https://github.com/Qinbf<br>
-
 # In[1]:
 
 import pandas as pd
